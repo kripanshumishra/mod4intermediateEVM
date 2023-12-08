b4ea463ce9fa8507d707062ec6234bf79d0850f5
kripanshu mishra <92199408+kripanshumishra@users.noreply.github.com>
2023-12-08 23:23:34 +0530

    Update mod4.sol

--- mod4.sol
+++ mod4.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OpenSkull is ERC20, Ownable {
+    mapping(address => bool) private hasOpened;
+    uint public constant totalOpened = 1000 * 10;
+
+    constructor(address initialOwner) ERC20("OpenSkull", "OSK") Ownable(initialOwner) {
+        _mint(initialOwner, totalOpened);
+    }
+
+    function mintHead(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
+
+    function claimHead() public {
+        require(balanceOf(msg.sender) >= totalOpened, "Insufficient balance");
+        require(!hasOpened[msg.sender], "Treasure already claimed");
+
+        hasOpened[msg.sender] = true;
+        _burn(msg.sender, totalOpened);
+    }
+
+    function burnHead(uint amount) public {
+        _burn(msg.sender, amount);
+    }
+}
 
